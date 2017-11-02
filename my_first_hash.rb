785524ce2e9d209904dcf859a5514dd68dfa24a0
Chris Cardle <cdcardle@gmail.com>
2017-11-02 13:52:19 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,11 +4,7 @@ end
 
 
 def shipping_manifest
-  shipping_manifest = {}
-  shipping_manifest["whale bone corsets"] = 5
-  shipping_manifest["porcelain vases"] =  2
-  shipping_manifest["oil paintings"] = 3
-  #shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
