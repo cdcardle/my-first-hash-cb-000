45060a5bdb160bfe992994a615fbae8624b69320
Chris Cardle <cdcardle@gmail.com>
2017-11-02 13:51:23 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,10 +4,11 @@ end
 
 
 def shipping_manifest
-  shipping_manifest = Hash.new
-  shipping_manifest["whale bone corset"] = 5
-  shipping_manifest["porcelain vases"] =  2
-  shipping_manifest["oil paintings"] = 3
+  #shipping_manifest = Hash.new
+  #shipping_manifest["whale bone corset"] = 5
+  #shipping_manifest["porcelain vases"] =  2
+  #shipping_manifest["oil paintings"] = 3
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
